4a8ce62bbb2439f00da62c2f227807b647a98788
Lazare Filor <lafilor@icloud.com>
2023-07-05 17:32:19 +0200

    Use single object for data storage

--- frontend/src/app/game/game-control/game-control.component.ts
+++ frontend/src/app/game/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {GameControl} from '../../shared/game-control';
 
 @Component({
@@ -6,23 +6,48 @@ import {GameControl} from '../../shared/game-control';
   templateUrl: './game-control.component.html',
   styleUrls: ['./game-control.component.scss']
 })
-export class GameControlComponent {
+export class GameControlComponent implements OnInit{
 
   @Input()
-  controlId?: string;
-
-  @Input()
-  controlName?: string;
-
-  @Input()
-  controlAssetUrl?: string;
+  controlData?: GameControl;
 
   @Output() controlSelectEvent = new EventEmitter<GameControl>();
 
+  isRevealed: boolean = false;
+
+  ngOnInit(): void {
+    //set isRevealed in case data is not default on start
+  }
+
+
+  /**
+   * Reveals new data to the user in case none had existed before
+   * @param data the new data that should be revealed
+   */
+  reveal(data: GameControl) {
+
+    if (this.isRevealed || !data) {
+      return;
+    }
+    this.controlData = data;
+    this.isRevealed = true;
+  }
+
+  /**
+   * Hides current data from the user
+   */
+  hide() {
+
+    if (!this.isRevealed) {
+      return;
+    }
+    this.isRevealed = false;
+  }
+
   selectControl() {
 
-    if (this.controlId !== undefined && this.controlName !== undefined && this.controlAssetUrl !== undefined) {
-      this.controlSelectEvent.emit({id: this.controlId, name: this.controlName, assetPath: this.controlAssetUrl});
+    if (this.isRevealed) {
+      this.controlSelectEvent.emit(this.controlData);
     }
   }
 }
