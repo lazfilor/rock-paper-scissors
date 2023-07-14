9239b63dbab6f9c7b92ed513a1fb1c5047c3bbe1
Lazare Filor <lafilor@icloud.com>
2023-07-14 20:14:10 +0200

    Use correct arch for backend image and add pass cors on all ports

--- backend/src/main/java/com/filor/rps/configuration/RpsDevelopmentCorsConfiguration.java
+++ backend/src/main/java/com/filor/rps/configuration/RpsDevelopmentCorsConfiguration.java
@@ -1,18 +1,21 @@
 package com.filor.rps.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+@Slf4j
 @Configuration
 @ConditionalOnProperty(name = "rps.dev.mode", havingValue = "true")
 public class RpsDevelopmentCorsConfiguration implements WebFluxConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        log.info("Allowing access from local angular app");
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("http://localhost:4200");
+                .allowedOrigins("*");
     }
 }
